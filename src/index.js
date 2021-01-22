f2da1c340309fde4e2bcd36de7b465a8f813e651
Anton Lyachek <lyachek.anton@gmail.com>
2021-01-23 03:56:18 +0500

    Added basic `paste` handler

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ const getModifiedStyles = (initial, modified) => {
   return modifiedRules.reduce((res, currRule, idx) => (currRule === initialRules[idx] ? res : [...res, currRule]), []).join(';');
 };
 
+const sanitizeText = (text) => text
+  .replace(/&/g, '&amp;')
+  .replace(/'/g, '&#x27;')
+  .replace(/"/g, '&quot;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+  .replace(/\//g, '&#x2F;');
+
 document.addEventListener('DOMContentLoaded', () => {
   const createStyledHeader = (elementType) => {
     const selectionRange = getSelectionRange();
@@ -26,6 +34,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const targetStyle = getModifiedStyles(defaultStyle, appliedStyle);
     newHeader.style = targetStyle;
   };
+
+  document.addEventListener('paste', (e) => {
+    e.preventDefault();
+
+    const htmlText = e.clipboardData.getData('text/html');
+    if (!htmlText) {
+      const text = sanitizeText(e.clipboardData.getData('text/plain'));
+      document.execCommand('insertHTML', false, text);
+    } else {
+
+    }
+  });
 
   textStyleButtons.forEach((button) => getElement(button).addEventListener('click', () => document.execCommand(button, false)));
   headerButtons.forEach((tag) => getElement(tag).addEventListener('click', () => createStyledHeader(tag)));
